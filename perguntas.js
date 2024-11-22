8849baf8690df60d213562344ec21f9c1f42d8ce
Geovanna0507 <00001103271246sp@al.educacao.sp.gov.br>
2024-11-22 16:18:08 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,53 @@
+criaCartao(
+    'História', 
+    'Quanto tempo durou a escravidão no Brasil?',
+    '388 anos'
+)
+
+criaCartao(
+    'História', 
+    'Quem foi que descobriu o Brasil?',
+    'O príncipe D. Pedro'
+)
+
+criaCartao(
+    'Matemática', 
+    'Quantas tipos de frações existem?',
+    '3 tipos: própria,omprópria e aparente'
+)
+
+criaCartao(
+    'Matemática',
+    'Qual o resultado de 18³?',
+    'O resultado é 18.18.18= 5.832'
+)
+
+criaCartao(
+    'Geografia',
+    'Quais são os estados da região centro-oeste?',
+    'Góias,Mato Grosso,Mato Grosso do Sul e o Distrito Federal'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é a capital de Santa Catarina?',
+    'A capital de Santa Catarina é Florianópolis'
+)
+
+criaCartao(
+    'Língua Portuguesa',
+    'Qual é o poeta mais famoso do Brasil?',
+    'Carlos Drummond de Andrade'
+)
+
+criaCartao(
+    'Língua Portuguesa',
+    'Qual é o poema mais famoso do Brasil?',
+    'Soneto de Fidelidade (1946) de Vinicius de Moraes'
+)
+
+criaCartao(
+    'Biologia',
+    'O que é Genes?',
+    'È um segmento de uma molécula de DNA responsável pelas características herdadas geneticamente'
+)
